bf57ec7a074e78e09a447624ba16b69807a12965
claesn <c.neuefeind@uni-koeln.de>
2017-10-18 11:13:34 +0200

    Add basic package.

---
+++ ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/Corpus.java
@@ -0,0 +1,18 @@
+package de.uni_koeln.spinfo.textengineering.ir.basic;
+
+public class Corpus {
+
+	/*
+	 *  TODO 
+	 *  
+	 *  1. Auf www.gutenberg.org die gesammelten Werke von Shakespeare als Textdatei (UTF-8) herunterladen (Dateiname "pg100.txt")
+	 *  
+	 *  2. Datei in Projektverzeichnis legen
+	 *  
+	 *  3. Einfachen Test für Korpuserstellung anlegen (in TestBasicIR.java)
+	 *  
+	 *  4. Datei einlesen
+	 *  
+	 */
+
+}
---
+++ ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/TestBasicIR.java
@@ -0,0 +1,18 @@
+package de.uni_koeln.spinfo.textengineering.ir.basic;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestBasicIR {
+
+	@BeforeClass
+	public static void setUp() {
+		// TODO Korpus einlesen
+	}
+
+	@Test
+	public void testCorpus() {
+		// TODO Testen, ob Korpus korrekt angelegt wurde
+	}
+
+}

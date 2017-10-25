501951ab309f1a34c27486b988d978f411c6e46e
claesn <c.neuefeind@uni-koeln.de>
2017-10-25 12:55:34 +0200

    Course templates.

--- ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/Corpus.java
+++ ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/Corpus.java
@@ -1,18 +1,46 @@
 package de.uni_koeln.spinfo.textengineering.ir.basic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Corpus {
 
-	/*
-	 *  TODO 
-	 *  
-	 *  1. Auf www.gutenberg.org die gesammelten Werke von Shakespeare als Textdatei (UTF-8) herunterladen (Dateiname "pg100.txt")
-	 *  
-	 *  2. Datei in Projektverzeichnis legen
-	 *  
-	 *  3. Einfachen Test für Korpuserstellung anlegen (in TestBasicIR.java)
-	 *  
-	 *  4. Datei einlesen
-	 *  
+	private String text;
+
+	public Corpus(String location) {
+
+		StringBuilder sb = new StringBuilder();
+		try {
+			Scanner scanner = new Scanner(new File(location));
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				sb.append(line);
+				sb.append("\n"); // explizites newline (für regex)
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		setText(sb.toString());
+		
+		//TODO: Corpusstruktur?
+		
+	}
+
+	/**
+	 * @return the text
 	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * @param text
+	 *            the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
 
 }
--- ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/TestBasicIR.java
+++ ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/TestBasicIR.java
@@ -1,18 +1,27 @@
 package de.uni_koeln.spinfo.textengineering.ir.basic;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestBasicIR {
 
+	private static Corpus corpus;
+
 	@BeforeClass
 	public static void setUp() {
-		// TODO Korpus einlesen
+		// Korpus einlesen
+		String filename = "pg100.txt";
+		corpus = new Corpus(filename);
 	}
 
 	@Test
 	public void testCorpus() {
-		// TODO Testen, ob Korpus korrekt angelegt wurde
+		// Testen, ob Korpus korrekt angelegt wurde
+		assertTrue("Korpus sollte mehr als 1 Werk enthalten", corpus != null);
 	}
 
 }

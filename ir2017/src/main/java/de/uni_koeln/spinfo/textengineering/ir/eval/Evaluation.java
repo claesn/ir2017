3144a178b17a92e5661aec3d83eeee6c4155c521
claesn <c.neuefeind@uni-koeln.de>
2018-01-17 15:57:36 +0100

    Seminar code 2018-01-17.

--- ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/eval/Evaluation.java
+++ ir2017/src/main/java/de/uni_koeln/spinfo/textengineering/ir/eval/Evaluation.java
@@ -1,5 +1,6 @@
 package de.uni_koeln.spinfo.textengineering.ir.eval;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -7,9 +8,10 @@ import java.util.List;
  */
 public class Evaluation {
 
+	private List<Integer> relevant;
 
 	public Evaluation(List<Integer> goldstandard) {
-		
+		this.relevant = goldstandard;
 	}
 
 	public EvaluationResult evaluate(List<Integer> retrieved) {
@@ -17,14 +19,24 @@ public class Evaluation {
 		 * Für das Suchergebnis müssen Precision, Recall und F-Maß errechnet werden. Grundlage sind die
 		 * "true positives" , die anhand des Goldstandards ermittelt werden.
 		 */
-		
-		//TODO
-		
-		double p = 0.0;
-		double r = 0.0;
-		double f = 0.0;
+		int tp = truePositives(retrieved);
+		int fp = retrieved.size() - tp;
+		int fn = relevant.size() - tp;
+
+		double p = (double) tp / (tp + fp);
+		double r = (double) tp / (tp + fn);
+		double f = 2 * p * r / (p + r);
 
 		return new EvaluationResult(p, r, f);
 	}
 
+	private int truePositives(List<Integer> retrieved) {
+		/*
+		 * Zur Ermittlung der true positives bilden wir die Schnittmenge der beiden Listen und erhalten dadurch die
+		 * Anzahl der gefundenen Elemente, die auch relevant sind:
+		 */
+		List<Integer> tp = new ArrayList<>(relevant);
+		tp.retainAll(retrieved);
+		return tp.size();
+	}
 }
